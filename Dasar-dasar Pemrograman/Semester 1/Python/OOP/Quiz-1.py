4e1002972de20fd436dc36c92c7e1608b1746322
Erlangga Riansyah <erlanggariansyah0@gmail.com>
2022-01-13 21:12:55 +0700

    a

--- Dasar-dasar Pemrograman/Semester 1/Python/OOP/Quiz-1.py
+++ Dasar-dasar Pemrograman/Semester 1/Python/OOP/Quiz-1.py
@@ -1,2 +1,16 @@
+import datetime
+from dateutil.relativedelta import relativedelta
+
 class Orang:
-    __
+    def __init__(self, nama, tgl_lahir):
+        self.nama = nama
+        self.tgl_lahir = tgl_lahir.split("-")
+
+    def umur(self):
+        now = datetime.datetime.now()
+        difference = relativedelta(now, self.tgl_lahir)
+        print("a")
+
+
+orang = Orang("Angga", "2003-09-27")
+orang.umur
