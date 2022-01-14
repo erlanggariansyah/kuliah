23f406e78d1ad31a1d3da3f70cd057f5198c473b
erlanggadw <erlangga.dewarangga@gmail.com>
2022-01-14 15:26:56 +0700

    API

--- Dasar-dasar Pemrograman/Semester 1/Python/OOP/Quiz-API.py
+++ Dasar-dasar Pemrograman/Semester 1/Python/OOP/Quiz-API.py
@@ -3,7 +3,8 @@ import requests, os
 
 class Rest:
     base_url = "https://api.kawalcorona.com/"
-
+    item = 10
+    
     def show_data(self, arg):
         if arg == 1:
             response = requests.get(self.base_url + "/indonesia").json()
@@ -22,18 +23,66 @@ class Rest:
             ans = input()
 
             if (ans == "ASC"):
-                for i in range(len(response)):
-                    print("\nKasus di", response[i]["attributes"]["Country_Region"])
-                    print("Positif", response[i]["attributes"]["Confirmed"])
-                    print("Meninggal", response[i]["attributes"]["Deaths"])
-                    print("Sembuh", response[i]["attributes"]["Recovered"])
+                def show(item):
+                    for i in range(self.item):
+                        print("\nKasus di", response[i]["attributes"]["Country_Region"])
+                        print("Positif", response[i]["attributes"]["Confirmed"])
+                        print("Meninggal", response[i]["attributes"]["Deaths"])
+                        print("Sembuh", response[i]["attributes"]["Recovered"])
+
+                show(self.item)
+
+                def item_f():
+                    print("\n'<' untuk ke halaman berikutnya, '>' untuk ke halaman selanjutnya. Ketik apa saja untuk keluar.")
+                    
+                    ans = input()
+
+                    if (ans == '>'):
+                        self.item = self.item + 10
+                        show(self.item)
+                        item_f()
+
+                    elif (ans == '<'):
+                        self.item = self.item - 10
+                        show(self.item)
+                        item_f()
+
+                    else:
+                        os.system("cls")
+                        print("Input tidak diterima!")
+
+                item_f()
 
             elif (ans == "DESC"):
-                for i in range(len(response)):
-                    print("\nKasus di", response[len(response) - i - 1]["attributes"]["Country_Region"])
-                    print("Positif", response[len(response) - i - 1]["attributes"]["Confirmed"])
-                    print("Meninggal", response[len(response) - i - 1]["attributes"]["Deaths"])
-                    print("Sembuh", response[len(response) - i - 1]["attributes"]["Recovered"])
+                def show(item):
+                    for i in range(10):
+                        print("\nKasus di", response[self.item - i - 1]["attributes"]["Country_Region"])
+                        print("Positif", response[self.item - i - 1]["attributes"]["Confirmed"])
+                        print("Meninggal", response[self.item - i - 1]["attributes"]["Deaths"])
+                        print("Sembuh", response[self.item - i - 1]["attributes"]["Recovered"])
+
+                show(self.item)
+
+                def item_f():
+                    print("\n'<' untuk ke halaman berikutnya, '>' untuk ke halaman selanjutnya. Ketik apa saja untuk keluar.")
+                    
+                    ans = input()
+
+                    if (ans == '>'):
+                        self.item = self.item + 10
+                        show(self.item)
+                        item_f()
+
+                    elif (ans == '<'):
+                        self.item = self.item - 10
+                        show(self.item)
+                        item_f()
+
+                    else:
+                        os.system("cls")
+                        print("Input tidak diterima!")
+
+                item_f()
 
             else:
                 os.system("cls")
