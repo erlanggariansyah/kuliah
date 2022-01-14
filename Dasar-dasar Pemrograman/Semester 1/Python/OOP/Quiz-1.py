edbbccfc1fd3cfd97269ffbcf74c320a3ef94984
erlanggadw <erlangga.dewarangga@gmail.com>
2022-01-14 08:52:21 +0700

    quiz-1 quiz-2

--- Dasar-dasar Pemrograman/Semester 1/Python/OOP/Quiz-1.py
+++ Dasar-dasar Pemrograman/Semester 1/Python/OOP/Quiz-1.py
@@ -1,16 +1,19 @@
+from datetime import date
 import datetime
+
+# membutuhkan library python-dateutil
 from dateutil.relativedelta import relativedelta
 
 class Orang:
     def __init__(self, nama, tgl_lahir):
         self.nama = nama
-        self.tgl_lahir = tgl_lahir.split("-")
+        raw = tgl_lahir.split("-")
+        self.tgl_lahir = datetime.datetime(int(raw[0]), int(raw[1]), int(raw[2]))
 
     def umur(self):
         now = datetime.datetime.now()
         difference = relativedelta(now, self.tgl_lahir)
-        print("a")
+        print(difference.years, "tahun.")
 
-
-orang = Orang("Angga", "2003-09-27")
-orang.umur
+a = Orang("Angga", "2003-09-27")
+a.umur()
