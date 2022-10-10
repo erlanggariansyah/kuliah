f26c7b7478b5ad0343e1bd37a095f6fb840b3ae7
Erlangga Riansyah <erlanggariansyah0@gmail.com>
2022-10-10 19:29:18 +0700

    a

---
+++ PDPL/Praktikum-1/src/com/erlangga/Main.java
@@ -0,0 +1,13 @@
+package com.erlangga;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Erlangga Riansyah");
+        System.out.println("TTL : Bogor, 27 September 2003");
+        System.out.println("Pekerjaan : Backend Developer");
+        System.out.println("Status : Mahasiswa");
+        System.out.println("Umur : 19");
+        System.out.println("Alamat : Jl. Raya Narogong, Kecamatan Cileungsi, Kabupaten Bogor, Jawa Barat 16820");
+    }
+}
