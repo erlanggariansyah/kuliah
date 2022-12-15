e0fd0b9b9214ddc0e55bfe681807157159ddeda1
Erlangga Riansyah <erlanggariansyah0@gmail.com>
2022-12-15 15:44:39 +0700

    Pert 10

--- Peminatan SE/Pemrograman Backend/Task-10/task.js
+++ Peminatan SE/Pemrograman Backend/Task-10/task.js
@@ -2,27 +2,34 @@
  * Fungsi untuk menampilkan hasil download
  * @param {string} result - Nama file yang didownload
  */
- function showDownload(result) {
-    console.log("Download selesai");
-    console.log("Hasil Download: " + result);
-  }
+const showDownload = (result) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      Math.random() < 0.5 ? resolve(result) : reject("Download Gagal.");
+    }, 3000)
+  })
+}
   
-  /**
-   * Fungsi untuk download file
-   * @param {function} callback - Function callback show
-   */
-  function download(callShowDownload) {
-    setTimeout(function () {
-      const result = "windows-10.exe";
-      callShowDownload(result);
-    }, 3000);
-  }
+/**
+* Fungsi untuk download file
+* @param {function} callback - Function callback show
+*/
+const download = async (callShowDownload) => {
+  await callShowDownload("windows-10.exe");
+}
   
-  download(showDownload);
+download(showDownload)
+.then((resolve) => {
+  console.info("Download Selesai.");
+  console.info(`Hasil Download: ${resolve}`);
+})
+.catch((reject) => {
+  console.error(reject);
+});
   
-  /**
-   * TODO:
-   * - Refactor callback ke Promise atau Async Await
-   * - Refactor function ke ES6 Arrow Function
-   * - Refactor string ke ES6 Template Literals
-   */
+/**
+* TODO:
+* - Refactor callback ke Promise atau Async Await
+* - Refactor function ke ES6 Arrow Function
+* - Refactor string ke ES6 Template Literals
+*/
