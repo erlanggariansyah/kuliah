858c030874c86167743cf70dc1170338032ec58c
Erlangga Riansyah <erlanggariansyah0@gmail.com>
2022-12-28 01:17:18 +0700

    Pertemuan 12 - Pemrograman Backend

---
+++ Peminatan SE/Pemrograman Backend/Task-12/data/students.js
@@ -0,0 +1,19 @@
+// TODO 1: Buat data students
+// TODO 2: export data students
+const students = [
+    {
+        "id": 1,
+        "nama": "Angga"
+    }, {
+        "id": 2,
+        "nama": "Anggi"
+    }, {
+        "id": 3,
+        "nama": "Aufa"
+    }, {
+        "id": 4,
+        "nama": "Ishfa"
+    }
+];
+
+module.exports = students;
---
+++ Peminatan SE/Pemrograman Backend/Task-12/models/Student.js
@@ -0,0 +1,40 @@
+const db = require("../config/database");
+
+class Student {
+    static last() {
+        return new Promise((resolve, reject) => {
+            db.query("SELECT * FROM students ORDER BY id DESC LIMIT 1", (e, r) => {
+                resolve(r);
+            })
+        })
+    }
+
+    static all() {
+        return new Promise((resolve, reject) => {
+            const query = "SELECT * FROM students";
+
+            db.query(query, (e, r) => {
+                resolve(r);
+            })
+        })
+    }
+
+    static async insert(req) {
+        // maaf kak serba manual, just keep the core concepts alive (padahal memang tidak tahu express :D)
+        const latestStudent = await this.last();
+        const id = latestStudent.id + 1;
+        const { nama, nim, email, jurusan } = req;
+
+        return new Promise((resolve, reject) => {
+            const query = `INSERT INTO students (id, nama, nim, email, jurusan, updated_at, created_at) VALUES
+            (\"${id}\", \"${nama}\", \"${nim}\", \"${email}\", \"${jurusan}\"   , NOW(), NOW())`;
+
+            db.query(query, (e, r) => {
+                if (e) console.error(e.message);
+                resolve(r);
+            })
+        })
+    }
+}
+
+module.exports = Student;
