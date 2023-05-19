45ec0d7772acf35fed30261f37080746f70fccd3
Erlangga Riansyah <erlanggariansyah0@gmail.com>
2023-05-20 00:11:16 +0700

    Pertemuan 8 - Task

--- Peminatan SE/Pemrograman Frontend/pertemuan-8/task/movie-database/src/components/functional/AddMovieForm.js
+++ Peminatan SE/Pemrograman Frontend/pertemuan-8/task/movie-database/src/components/functional/AddMovieForm.js
@@ -3,33 +3,56 @@ import { useState } from "react";
 
 const AddMovieForm = (props) => {
     const { container, form, form__field, form__left, form__right, form__title, form__image, form__button, form__subtitle } = styles;
-    const [ title, setTitle ] = useState("")
-    const [ date, setDate ] = useState("")
-    const [ link, setLink ] = useState("")
-    const [ genre, setGenre ] = useState("")
-    const [ isTitleNull, setIsTitleNull ] = useState(false)
-    const [ isDateNull, setIsDateNull ] = useState(false)
-    const [ isLinkNull, setIsLinkNull ] = useState(false)
-    const [ isGenreNull, setIsGenreNull ] = useState(false)
+    const [ formData, setFormData ] = useState({
+        title: "",
+        date: "",
+        link: "",
+        genre: ""
+    })
+
+    const [ formValidation, setFormValidation ] = useState({
+        titleNull: false,
+        dateNull: false,
+        linkNull: false,
+        genreNull: false
+    })
+
+    const { title, date, link, genre } = formData;
+    const { titleNull, dateNull, linkNull, genreNull } = formValidation;
 
     const { movies, setMovies } = props;
     const validate = () => {
         if (title === "") {
-            setIsTitleNull(true);
+            setFormValidation({
+                ...formValidation,
+                titleNull: true
+            })
+
             return false;
         } else if (date === "") {
-            setIsDateNull(true);
-            setIsTitleNull(false);
+            setFormValidation({
+                ...formValidation,
+                dateNull: true
+            })
+            
             return false;
-        } else if (poster === "") {
-            setIsLinkNull(true);
-            setIsDateNull(false);
+        } else if (link === "") {
+            setFormValidation({
+                ...formValidation,
+                linkNull: true
+            })
+            
             return false;
-        } else {
-            setIsTitleNull(false);
-            setIsDateNull(false);
+        } else if (genre === "") {
+            setFormValidation({
+                ...formValidation,
+                genreNull: true
+            })
+            
             return false;
-        }
+        } 
+        
+        return true;
     }
 
     const add = () => {
@@ -50,8 +73,13 @@ const AddMovieForm = (props) => {
         validate() && add();
     }
 
-    const handleChange = () => {
+    const handleChange = (e) => {
+        const { name, value } = e.target;
 
+        setFormData({
+            ...formData,
+            [name]: value
+        })
     }
     
     const button = <button className={form__button} type="submit">Submit</button>
@@ -63,25 +91,25 @@ const AddMovieForm = (props) => {
     const movieForm = (
             <div>
                 <form onSubmit={handleClick}>
-                    { isTitleNull ? <p>Title wajib di isi.</p> : null }
+                    { titleNull ? <p>Title wajib di isi.</p> : null }
                     <div className={form__field}>
                         {movieTitle}
                         <input type="text" title={movieTitle} name="title" onChange={handleChange} />
                     </div>
-                    { isDateNull ? <p>Date wajib di isi.</p> : null }
+                    { dateNull ? <p>Date wajib di isi.</p> : null }
                     <div className={form__field}>
                         {movieYear}
-                        <input type="text" title={movieYear} name="year" onChange={handleChange} />
+                        <input type="text" title={movieYear} name="date" onChange={handleChange} />
                     </div>
-                    { isLinkNull ? <p>Link wajib di isi.</p> : null }
+                    { linkNull ? <p>Link wajib di isi.</p> : null }
                     <div className={form__field}>
                         {movieLink}
                         <input type="text" title={movieLink} name="link" onChange={handleChange} />
                     </div>
-                    { isGenreNull ? <p>Genre wajib di isi.</p> : null }
+                    { genreNull ? <p>Genre wajib di isi.</p> : null }
                     <div className={form__field}>
                         {movieGenre}
-                        <select value={genre} onChange={handleChange}>
+                        <select name="genre" value={genre} onChange={handleChange}>
                             <option value="Action" selected>Action</option>
                             <option value="Drama" selected>Drama</option>
                             <option value="Horror" selected>Horror</option>
